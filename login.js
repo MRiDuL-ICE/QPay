54c58ded6c98edb11d9207c73f297e39fecbfde2
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-09-19 11:18:33 +0600

    Qpay home page

--- login.js
+++ login.js
@@ -2,6 +2,17 @@ document.getElementById('button-login').addEventListener('click', function(event
     event.preventDefault();
     console.log('login button clicked');
 
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value);
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('login-pin').value;
+    
+    console.log(phoneNumber);
+    console.log(pinNumber);
+
+    if(phoneNumber==='01303579271' && pinNumber==='1234'){
+        console.log('Log in success')
+        window.location.href = '/home.html';
+    }
+    else{
+        console.log('phone or pin is incorrect');
+    }
 })
